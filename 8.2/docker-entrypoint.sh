d8c2efc1b706decf551658e5d15f47f3a14cd927
Culy247 <diep.pham@amela.vn>
2023-03-07 08:13:57 +0700

    Change entry point

--- 8.2/docker-entrypoint.sh
+++ 8.2/docker-entrypoint.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$WWWUSER" ]; then
-    usermod -u $WWWUSER app
-fi
+exec /usr/bin/supervisord
 
-if [ ! -d /.composer ]; then
-    mkdir /.composer
-fi
-
-chmod -R ugo+rw /.composer
-
-if [ $# -gt 0 ]; then
-    exec gosu $WWWUSER "$@"
-else
-    exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-fi
